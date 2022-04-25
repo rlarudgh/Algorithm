c1b96f77ea8368eb624595cd15c48aeaefa98419
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-04-25 23:39:00 +0900

    [Bronze IV] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a * (b%10));
+    printf("%d\n", a* (b/10%10));
+    printf("%d\n", a * (b/(10*10)));
+    printf("%d\n", a * (b/(10*10))*100 + a* (b/10%10)*10+ a * (b%10));
+    return 0;
+}
