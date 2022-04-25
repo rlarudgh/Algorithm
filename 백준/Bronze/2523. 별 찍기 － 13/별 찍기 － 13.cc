ce67974137f05a25673e012dcaec86cb4df61b3c
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-04-25 23:35:05 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+int main() {
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i+1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i; j < n-1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
