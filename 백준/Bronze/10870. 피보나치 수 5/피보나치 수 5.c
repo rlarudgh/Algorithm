092cdd07942134630a8532cfcc8b5da022430c64
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-06-15 22:46:19 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int fibo(int num) {
+    if(num == 0)
+        return 0;
+    if(num == 1)
+        return 1;
+    return fibo(num-2) + fibo(num-1);
+}
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    printf("%d",fibo(num));
+}
