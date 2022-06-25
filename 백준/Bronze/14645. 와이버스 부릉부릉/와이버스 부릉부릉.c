467e8c5dcda66736f2bc8d7cddc232e702727251
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-06-25 17:31:26 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, a, b;
+    scanf("%d %d",&n,&a);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d",&a,&b);
+    }
+    printf("비와이\n");
+    return 0;
+}
