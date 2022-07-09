1350afa9438591e17167e68150ab022f93338a17
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-07-09 19:47:49 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 1504 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+#define MAX 100001
+#define true 1
+#define false 0
+
+int heap[MAX], size = 0;
+
+void ins(int x){
+    int i = ++size;
+    while(i != 1 && x > heap[i /2]){
+        heap[i] = heap[i / 2];
+		i /= 2;
+    }
+    heap[i] = x;
+}
+
+int delHeap(){
+    if(size == 0) return 0; // 값이 비어있을때 
+    int last = heap[1], p = 1, c;
+    heap[1] = heap[size--];
+    while(true){
+        c = p * 2;
+        if(c + 1 <= size && heap[c] <heap[c + 1]) c++;
+        if(c > size || heap[c] < heap[p]) break;
+        int temp = heap[p];
+        heap[p] = heap[c];
+        heap[c] = temp;
+        p = c;
+    }
+    return last;
+}
+
+int main(){
+    int n, data;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &data);
+        if(data == 0) printf("%d\n", delHeap());
+        else ins(data);
+    }
+    return 0;
+}
