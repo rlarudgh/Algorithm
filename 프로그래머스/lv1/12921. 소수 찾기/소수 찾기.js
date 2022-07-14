48893b3f8e509eeafb2ca620fa9dd5a6ffd4d5af
KimKyeongHo <81161675+kimkh05@users.noreply.github.com>
2022-07-14 22:08:33 +0900

    [level 1] Title: 소수 찾기, Time: 63.31 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,10 +1,8 @@
 const solution = (n) => {
     let arr = Array(n+1).fill(true).fill(false, 0, 2);
     for(let i = 0; i <=n; i++){
-        if(arr[i]){
-            for(let j = i*i; j <=n; j+=i){
+        if(arr[i]) for(let j = i*i; j <=n; j+=i){
                 arr[j] = false;
-            }
         }
     }
     return arr.filter(e => e).length;
