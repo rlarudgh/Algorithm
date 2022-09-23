bd0a9d2895838fe937e7120a5ea30adc7071866b
김경호 <81161675+kimkh05@users.noreply.github.com>
2022-09-23 11:26:12 +0900

    [level 1] Title: 행렬의 덧셈, Time: 32.20 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,9 +1,3 @@
+import numpy as np
 def solution(arr1, arr2):
-    answer = []
-    for i in range(len(arr1)):
-        temp = []
-        for j in range(len(arr1[i])):
-            temp.append(arr1[i][j] + arr2[i][j])
-        answer.append(temp)
-            
-    return answer
+    return (np.array(arr1) + np.array(arr2)).tolist()
