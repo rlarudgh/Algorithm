48873c9e2069cbf184e10c1cd2896d0f9fb8f8d7
kimkh05 <81161675+kimkh05@users.noreply.github.com>
2023-03-28 13:51:18 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,11 +1,12 @@
-function solution(citations) {
+const solution = citations => {
     let answer = 0;
     citations.sort((a, b) => b - a);
-    console.log(citations)
+
     for (let i = 0; i < citations.length; i++) {
         if (i < citations[i]) {
             answer++;
         }
     }
+    
     return answer;
 }
