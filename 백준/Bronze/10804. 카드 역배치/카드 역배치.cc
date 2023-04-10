db1d25b2ecb5c6ef99abfcb9791efb663f002b03
kimkh05 <81161675+kimkh05@users.noreply.github.com>
2023-04-10 11:11:56 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+#define MAX_SIZE 21
+
+void swap(int* a, int* b) {
+   int temp =  *a;
+   *a = *b;
+   *b = temp; 
+}
+
+int main() {
+    int start, end;
+    int card[MAX_SIZE] = {0,};
+    
+    for(int i = 0; i < MAX_SIZE; i++) {
+        card[i] =  i;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        cin >> start >> end;
+        for(int j = 0, times = (end - start + 1) / 2; j < times; j++) {
+            swap(&card[start + j], &card[end - j]);
+        }
+    }
+
+    for(int i = 1; i < MAX_SIZE; i++) {
+      cout << card[i] << ' ';
+    }
+  
+    return 0;
+}
